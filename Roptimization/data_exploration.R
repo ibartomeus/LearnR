6a037460809bbab08c1d1b4c5f24025bb4133e72
ibartomeus <ibartomeus@home>
2016-09-12 17:21:34 +0200

    initial idea to force students to remember to plot your data always.

---
+++ Roptimization/data_exploration.R
@@ -0,0 +1,22 @@
+#Data exploration
+
+#Read this dataset and explore it.
+
+#read data
+dat <- read.csv("Roptimization/data/extinction.csv", h = TRUE)
+
+head(dat)
+str(dat)
+summary(dat)
+hist(dat$body_size)
+hist(dat$extinction_risck)
+cor.test(dat$extinction_risck, dat$body_size)
+m <- lm(dat$extinction_risck ~ dat$body_size)
+summary(m)
+plot(dat$extinction_risck ~ dat$body_size)
+abline(m)
+plot(m)
+
+#take home message. ALways plot your data
+
+#example ascombe quartet?
