d593b570f2b14ed729f502799bf2248f0d4c1fa1
ibartomeus <ibartomeus@home>
2016-09-20 17:12:44 +0200

    Add quick idea on making the students debug some code that fails. Not fully implemented

--- Roptimization/data_exploration.R
+++ Roptimization/data_exploration.R
@@ -21,3 +21,26 @@ scatter.smooth(dat$extinction_risck ~ dat$body_size)
 #take home message. ALways plot your data
 
 #example ascombe quartet?
+
+
+#debug this function
+#learn how to debug, split in pieces the problem, seacrh in stack overflow, learn diferent fixes are always available
+
+#make one of those cases when drop = FALSE is needed.
+
+dataset <- data.frame(groups = c(rep("a",4), "b", rep("c",5)),
+                      var = rnorm(10))
+
+col_means <- function(df) {
+  data.frame(lapply(df, mean))
+}
+
+
+colMeans(mtcars[, "mpg"])
+col_means(mtcars)
+col_means(mtcars[, 0])
+col_means(mtcars[0, ])
+col_means(mtcars[, "mpg"])
+#needed
+col_means(mtcars[, "mpg", drop = FALSE])
+
