b9cbd7756af4df60f3b4e0af23b85cc25cb74f2e
ibartomeus <ibartomeus@home>
2016-11-16 17:54:38 +0100

    multivariante

--- Roptimization/multivariante.R
+++ Roptimization/multivariante.R
@@ -1,10 +1,49 @@
-Análisis multivariante (paquetes vegan y mvabund)	
+#This scripts introduce multivariate analysis
 
-http://environmentalcomputing.net/introduction-to-mvabund/
-  
-  (and maybe: http://www.davidzeleny.net/anadat-r/doku.php/en:pcoa_nmds)
+library(vegan)
 
-https://jonlefcheck.net/2012/10/24/nmds-tutorial-in-r/
+
+#The data----
+Herbivores <- read.csv(file = "data/Herbivore_specialisation.csv", header = TRUE)
+
+#color codes
+Habitat <- Herbivores$Habitat
+DayNight <- Herbivores$DayNight
+
+#select the community
+Herb_community <- Herbivores[,5:11]
+
+
+Herb_community.mds <- metaMDS(comm = Herb_community, distance = "bray", trace = FALSE, autotransform = FALSE)
+plot(Herb_community.mds$points, col = Habitat, pch = 16) 
+
+plot(Herb_community.mds$points, col = Habitat, pch = 16)
+Habitat.uni <- unique(Habitat)
+legend(x = -2.5, y = -1.5, Habitat.uni, pch=16, col = 1:5, cex = 0.8)
+
+plot(Herb_community.mds$points, col = DayNight, pch = 16) 
+legend(x = -2.5, y = -1.5, c("Day","Night"), pch=16, col = 1:5, cex = 0.8)
+
+#assumptions:
+Herb_community.mds$stress
+
+#If the stress value is greater than 0.2, it is advisable to include an 
+#additional dimension, but remember that human brains are not very well 
+#equipped to visualise objects in more than 2-dimensions.
+
+#Transformation and standardisation. Transforming data sets prior to 
+#creating a MDS plot is often desirable, not to meet assumptions of normality, 
+#but to reduce the influence of extreme values. For example,
+
+Herb_community.sq <- sqrt(Herb_community)
+Herb_community.sq.mds <- metaMDS(comm = Herb_community.sq, distance = "bray", trace = FALSE)
+plot(Herb_community.sq.mds$points, col = Habitat, pch = 16) 
+
+
+#Further reading:
+#http://environmentalcomputing.net/introduction-to-mvabund/
+#http://www.davidzeleny.net/anadat-r/doku.php/en:pcoa_nmds)
+#https://jonlefcheck.net/2012/10/24/nmds-tutorial-in-r/
   
   
   # Non-metric multidimensional scaling (NMDS) is one tool commonly used to 
